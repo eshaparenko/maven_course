d0713c6b041cd9737349a0cabbc68942bf98b93d
shaparen <ext-eugene.shaparenko@here.com>
2019-10-23 00:13:22 +0300

    playing with properties

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ export M2=$M2_HOME/bin
 export PATH=$M2:$PATH 
 mvn -version
 ```
+
+# Generate maven project template
+```
+mvn archetype:generate
+```
+
+#Use different profiles
+```
+mvn clean package -P prod
+mvn clean package -Denv=prod
+
+#Run app
+java -jar target/firstTuturial-1.0-SNAPSHOT.jar
+```
---
+++ firstTuturial/src/main/java/shapatest/App.java
@@ -0,0 +1,34 @@
+package shapatest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        Properties prop = app.loadPropertiesFile("config.properties");
+        prop.forEach((k, v) -> System.out.println(k + ":" + v));
+
+    }
+
+    public Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+
+        return prop;
+
+    }
+}
